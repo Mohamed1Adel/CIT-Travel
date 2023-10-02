50f48d9a7ee56c25d27da6ded43ea4541292dc9d
Mohamed Adel <mohamedqwe542@gmail.com>
2023-10-02 19:20:02 +0300

    r

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="navbar-bg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstra</Navbar.Brand>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
