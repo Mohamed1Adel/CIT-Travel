7f4895201f4d6df91c8c711c82701b5ef4dd6ee4
mohamed <mohamedqwe542@gmail.com>
2023-10-02 09:55:55 +0300

    1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="http://wa.me/+201551563900">whatsApp</Nav.Link>
+            <Nav.Link href="http://wa.me/+201551563900">whatsAppp</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
             <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
