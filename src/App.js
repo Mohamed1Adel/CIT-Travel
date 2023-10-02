00f8643356fee606a6d7c2b5ec5fe87fbfa67541
Mohamed Adel <mohamedqwe542@gmail.com>
2023-10-02 09:56:44 +0300

    change navbar style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
 function App() {
   return (
     <div className="">
       <NavBar />
+      <Landing/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={""} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
