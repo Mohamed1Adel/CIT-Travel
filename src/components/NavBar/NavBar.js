b358d058be2746f54f2629bc4c52e0078359904c
mohamed <mohamedqwe542@gmail.com>
2023-10-03 19:17:30 +0300

    o

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,12 +12,14 @@ function NavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav ">
           <Nav className="mx-5">
-            <Nav.Link href="http://wa.me/+201551563900">whatsApp</Nav.Link>
-            <Nav.Link href="/egypt">Egypt</Nav.Link>
+            <Nav.Link href="http://wa.me/+201551563900">Home</Nav.Link>
+            <Nav.Link href="/egypt">Visit Egypt</Nav.Link>
+            <Nav.Link href="">Domestic</Nav.Link>
+            <Nav.Link href="">Outbound</Nav.Link>
+            <Nav.Link href="">Hajj & Umrah</Nav.Link>
             <Nav.Link href="">Pricing</Nav.Link>
-            <Nav.Link href="">Pricing</Nav.Link>
-            <Nav.Link href="">Pricing</Nav.Link>
-            <Nav.Link href="">Pricing</Nav.Link>
+            <Nav.Link href="">Transportation</Nav.Link>
+            <Nav.Link href="">C.I.T Egypt</Nav.Link>
             <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
