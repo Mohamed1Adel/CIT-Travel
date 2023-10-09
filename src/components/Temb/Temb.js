4621ffaf4b01e6666b5020787ee2d6f09ac3f328
Mohamed Addl <mohamedqwe542@gmail.com>
2023-10-09 11:09:05 +0300

    u

--- src/components/Temb/Temb.js
+++ src/components/Temb/Temb.js
@@ -34,7 +34,7 @@ function Temb({ id, title, rate, decs, category, location }) {
           </div>
           {decs} Desert rose is the best seller hotel and best choise to you
         </Card.Text>
-        <Button style={{ width: "120px" }} variant="primary">
+        <Button style={{ width: "130px" }} variant="primary">
           <Link to={"/tembDetails/" + id}>
             Book Now <FontAwesomeIcon icon={faArrowRight} />
           </Link>
