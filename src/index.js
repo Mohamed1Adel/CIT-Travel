0fd83381a43affb9ca87a7918a2f9e80619814a1
mohamed <mohamedqwe542@gmail.com>
2023-10-24 15:49:23 +0300

    c

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <>
-          <BrowserRouter>
-    <App />
-          </BrowserRouter>
-    </>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
